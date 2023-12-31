5ef7a9b0ad4bbddc12591a5fc383a5979acdc620
ragestan <115482936+ragestan@users.noreply.github.com>
2023-12-31 19:42:48 +0000

    ayaya

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -4,5 +4,22 @@
 template <typename T>
 class MutantStack: public std::stack<T>
 {
-    
+    public:
+			MutantStack(void) : std::stack<T>() {};
+			MutantStack(MutantStack const & src) : std::stack<T>(src)
+			{
+				*this = src;
+			};
+			MutantStack		&operator=(MutantStack const &src) {
+				std::stack<T>::operator=(src);
+				return *this;
+			}
+			~MutantStack() {};
+            			typedef typename std::stack<T>::container_type::iterator iterator;
+			iterator	begin(void) {
+				return this->c.begin();
+			}
+			iterator	end(void) {
+				return this->c.end();
+			}
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,33 @@
 #include "MutantStack.hpp"
 
-int main()
+int main(void)
 {
-    std::stack<int> mystack;
-    mystack.push(1);
-    mystack.push(2);
-    mystack.push(3);
-    mystack.push(4);
+	MutantStack<int> mstack;
 
-    return 0;
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "Top number: " << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << "Stack size: " << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return (0);
 }
